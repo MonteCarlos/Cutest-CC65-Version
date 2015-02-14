fe0dda4b74e5ecd7d1e735905d96d9d6857502cf
SA <s.andree@lmtb.de>
2015-02-14 15:44:31 +0100

    added new array test with callback function and function which generates report string

---
+++ CuTestAssertArrayEqualsStepFunc.c
@@ -0,0 +1,40 @@
+
+#include "CuTest_internal.h"
+
+bool CuAssertArrayEqualsStepFunc_LineMsg(CuTest* tc, const char* file, int line, const char* message,
+	bool (*stepfunc)(size_t index, void* expected), void* actual, size_t elementsize, size_t len)
+{
+    size_t i=0,j=0;
+    size_t arrayIndex = 0;
+	void* expected = CuAlloc(elementsize);
+
+    if (0 == elementsize) {
+        CuFail_Line(tc, file, line, "0==elementsize", NULL);
+        return true;
+    }
+    if (0 == len) {
+        CuFail_Line(tc, file, line, "0==len", NULL);
+        return true;
+    }
+
+    for (j = 0; j < len; ++j){
+        for (i = 0; i < elementsize; ++i){
+			if ( !stepfunc(j, expected) ){
+				CuFail_Line(tc, file, line, "Callback failed at pos ", NULL);
+				CuTestAppendMessage(tc, "%d.", j);
+				goto endfunc;
+			}
+            if ( ((uint8_t*)expected)[arrayIndex] != ((uint8_t*)actual)[arrayIndex] ){
+                CuFail_Line(tc, file, line, "Arrays not equal at pos ", NULL);
+                CuTestAppendMessage(tc, "%d.", j);
+                goto endfunc;
+            }
+            arrayIndex += 1;
+        }
+    }
+	endfunc:
+
+	CuFree(expected);
+	return false;
+}
+
---
+++ CuTestGenerateReportString.c
@@ -0,0 +1,11 @@
+#include "CuTest_internal.h"
+
+CuString *CuTestGenerateReportString(CuSuite *suite){
+	CuString *str = CuStringNew();
+	size_t count = suite->count, fails = suite->failCount;
+	CuStringAppendFormat(str,"Runs:%u, Passes:%u, Fails:%u\n", count, count-fails, fails);
+	return str;
+}
+
+
+
