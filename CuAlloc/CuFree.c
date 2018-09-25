818aa0bfa03b13a03dedafbea0252621831080ce
mc78 <mc78@outlook.de>
2018-09-25 12:41:27 +0200

    Added assertions to CuFree

--- CuAlloc/CuFree.c
+++ CuAlloc/CuFree.c
@@ -1,18 +1,24 @@
 #include "CuAlloc_internal.h"
 
 bool CuFree(void *ptr){
+    assert (ptr); //makes no sense to pass NULL ptr to free
     if (ptr){
-		CuAlloc_t *ptr2 = CuAlloc_getHeaderAddr(ptr);
-		//(uintptr_t)ptr -= sizeof(CuAlloc_BufHeader_t);
-		//assert( ptr2 -> isvalid );
-		if ( !CuAlloc_getBufferValidity(ptr) )  return false;
-	//    if ( ptr2 -> this != ptr2) return false;
-		ptr2 -> dataOffset = 2*offsetof(CuAlloc_t, data);//invalidate buffer
-		free(ptr2);//only free on ptr2 because data is stored in var len arr, which is allocated together with the struct
-		++freecount;
-		lastFreed[0] = ptr2;
+        assert (CuAlloc_getBufferValidity(ptr)); // assert, that valid buffer is passed
+        if ( !CuAlloc_getBufferValidity(ptr) ){
+            return false;
+        }else{
+            CuAlloc_t *ptr2 = CuAlloc_getHeaderAddr(ptr);
+            //(uintptr_t)ptr -= sizeof(CuAlloc_BufHeader_t);
+            //assert( ptr2 -> isvalid );
 
-		return true;
+        //    if ( ptr2 -> this != ptr2) return false;
+            ptr2 -> dataOffset = 2*offsetof(CuAlloc_t, data);//invalidate buffer
+            free(ptr2);//only free on ptr2 because data is stored in var len arr, which is allocated together with the struct
+            ++freecount;
+            lastFreed[0] = ptr2;
+
+            return true;
+        }
 	}
 	return false;
 }
