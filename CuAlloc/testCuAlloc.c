9b8cfbd52364593a4e89a3b00061c35f4a549fcc
mc78 <mc78@outlook.de>
2018-09-28 15:54:21 +0200

    Copied makefile from CuAlloc also to CuTest and CuString dirs. Still need to adapt makefiles.

--- CuAlloc/testCuAlloc.c
+++ CuAlloc/testCuAlloc.c
@@ -232,7 +232,7 @@ int main(void){
 
     CuSuite_t* suite = NULL; //
     suite = CuAlloc_requestEarlyTests();
-    CuSuiteRun(suite);
+    CuSuiteRun(sui*te);
     CuSuiteDetails(suite, stdout);
     Nofails = CuSuiteGetFailcount(suite);
     CuSuiteDelete(suite);
